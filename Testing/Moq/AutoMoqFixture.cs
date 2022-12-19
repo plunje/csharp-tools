d5477fcbc2dc07cc84f9d3ad8b940155034bb228
Jaap van Vliet <j.van.vliet@rijkzwaan.nl>
2022-12-19 15:28:54 +0100

    AutoMoqFixture and ItExt

---
+++ Testing/Moq/AutoMoqFixture.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Kernel;
+
+namespace CSharpTools.Testing.Moq;
+
+public class AutoMoqFixture : Fixture
+{
+    public AutoMoqFixture(int repeatCount = 3)
+    {
+        RepeatCount = repeatCount;
+
+        Customize(new AutoMoqCustomization());
+        Behaviors.Remove(new ThrowingRecursionBehavior());
+        Behaviors.Add(new OmitOnRecursionBehavior());
+
+        Customizations.Add(new BoolAlwaysTrueBuilder());
+    }
+
+    private class BoolAlwaysTrueBuilder : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            var pi = request as PropertyInfo;
+
+            if (pi == null)
+            {
+                return new NoSpecimen();
+            }
+
+            if (pi.PropertyType == typeof(bool))
+            {
+                return true;
+            }
+
+            return new NoSpecimen();
+        }
+    }
+}
+
---
+++ Testing/Moq/ItExt.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using FluentAssertions.Equivalency;
+using Moq;
+
+namespace CSharpTools.Testing.Moq;
+
+public static class ItExt
+{
+    public static T AsMatch<T>(this T @object)
+    {
+        return AsMatch(@object, config => config);
+    }
+
+    public static T AsMatch<T>(this T @object, Func<EquivalencyAssertionOptions<T>, EquivalencyAssertionOptions<T>> config)
+    {
+        return Match.Create<T>(x =>
+        {
+            try
+            {
+                @object.Should().BeEquivalentTo(x, config);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        });
+    }
+}
